5990725459e38261fbde80b4cf893414bc1a0c77
David Luo <david.luo1996@hotmail.com>
2016-01-17 03:59:40 -0500

    added database scripts

---
+++ Database Scripts/createBudgetTable.sql
@@ -0,0 +1,9 @@
+/* Query executed on January 17, 2016 at 3:04:40 AM */
+create table Budget(
+	userID					Int Unique,
+	budgetTimeFrameType   VARCHAR(1),
+	budgetStartDate 		date,
+	budgetEndDate			date,
+	budgetValue    		Float,
+	PRIMARY KEY(userID)
+)
