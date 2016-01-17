fd54a5390e771cbd2e00b44a4ff3acb48c679db9
Richie Shi <richie_shi@hotmail.com>
2016-01-17 01:04:40 -0500

    Repush

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+app.engine('ejs', require('ejs').__express);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
 app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(path.join(__dirname, 'public')));
+//app.use(require('/controllers/main'))
+//app.use(express.urlencoded());
+//app.use(express.json());
 
 app.get('/', function(req, res) {
 	res.render('index');
+});
+
+app.post('/login', function(req, res) {
+	console.log(JSON.stringify(req.body));
 });
 
 app.listen(app.get('port'), function() {
